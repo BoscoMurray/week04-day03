ce9c6324da69b6a4e0bad7044921df0e62a915d0
BoscoMurray <rossamurray@gmail.com>
2017-06-14 18:37:10 +0100

    dropdown from db done

--- controller.rb
+++ controller.rb
@@ -2,6 +2,7 @@ require ('sinatra')
 require ('sinatra/contrib/all')
 require ('pry-byebug')
 require_relative ('models/student')
+require_relative ('models/house')
 
 #INDEX
 get "/students" do
@@ -11,13 +12,13 @@ end
 
 #NEW
 get "/students/new" do
+  @houses = House.all
   erb(:new)
 end
 
 #CREATE
 post "/students" do
   @student = Student.new(params)
-  # binding.pry
   @student.save()
   erb(:create)
 end
